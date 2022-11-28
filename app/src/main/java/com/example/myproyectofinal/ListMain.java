27a1b6cbe42e0d2948556826f74fa5d8660740bd
HoracioZG <zaldivar.galvan.horacio@gmail.com.com>
2022-11-27 19:50:04 -0600

    V1

---
+++ app/src/main/java/com/example/myproyectofinal/ListMain.java
@@ -0,0 +1,14 @@
+package com.example.myproyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_main);
+    }
+}
---
+++ app/src/main/java/com/example/myproyectofinal/Website.java
@@ -0,0 +1,55 @@
+package com.example.myproyectofinal;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.example.myproyectofinal.Json.Info;
+import com.example.myproyectofinal.Json.Json;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Website extends AppCompatActivity {
+
+    TextView textview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_website);
+        textview = findViewById(R.id.textView);
+
+        try {
+
+            int numArchivo = getIntent().getExtras().getInt("numArchivo");
+            Json json = new Json();
+
+            BufferedReader file = new BufferedReader(new InputStreamReader(openFileInput("Archivo" + numArchivo + ".txt")));
+            String lineaTexto = file.readLine();
+            String completoTexto = "";
+            while(lineaTexto != null){
+                completoTexto = completoTexto + lineaTexto;
+                lineaTexto = file.readLine();
+            }
+            Info datos = json.leerJson(completoTexto);
+            file.close();
+
+            textview.setText("Welcome " + datos.getFirstName());
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            new Handler( ).postDelayed(new Runnable() {
+                @Override
+                public void run(){
+                    Intent intent = new Intent( Website.this, ListMain.class);
+                    intent.putExtra("numArchivo", numArchivo);
+                    startActivity( intent );
+                }
+            } , 4000 );
+        }catch(Exception e){}
+    }
+}
